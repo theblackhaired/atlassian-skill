a36055f5f9fa2140833ceb53d3347b214598759e
Kirill Gorosov <kirillgorosov1996@gmail.com>
2026-02-19 02:58:37 +0300

    chore: remove leftover .devcontainer and .github dirs

--- .devcontainer/post-create.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-set -xe
-
-uv venv
-source .venv/bin/activate
--- .devcontainer/post-start.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -xe
-
-source .venv/bin/activate
-
-uv sync --frozen --all-extras --dev
-pre-commit install
